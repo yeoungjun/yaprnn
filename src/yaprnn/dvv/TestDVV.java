949f51b082e3267e831072f41c13eed28aa71051
mpgi3@arcor.de <mpgi3@arcor.de@0986f430-494d-0410-a540-1573547e6c47>
2008-06-15 21:04:09 +0000

    Erste Version des Subsampling für Bilder Hinzugefügt.

--- src/yaprnn/dvv/TestDVV.java
+++ src/yaprnn/dvv/TestDVV.java
@@ -1,31 +1,71 @@
 package yaprnn.dvv;
 
 import java.util.*;
+import yaprnn.mlp.*;
 
 class TestDVV {
+
+	static TangensHyperbolicus tanh = new TangensHyperbolicus();
 	
 	public static void main(String[] args) throws InvalidFileException {
 		TestDVV.test01();
 		TestDVV.test02();
+		TestDVV.test03();
+		//TestDVV.test();
 	}
 
 	public static void test01() {
-		byte[][] image = { {1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1} };
-		IdxPicture pic = new IdxPicture(image, "1", "a", 0);
-		pic.subsample(0, 0, new yaprnn.mlp.TangensHyperbolicus());
-		double[] data = pic.getData();
-		for(int i=0; i<image.length; i++)
-			for(int j=0; j<image[0].length; j++)
-				if(data[i*image[0].length + j] != Math.tanh((double)image[i][j]))
-					System.out.println("data scaled incorrectly");
-		System.out.println("Test 01 OK");
+		byte[][] img = new byte[1][1];
+		img[0][0] = 100;
+		IdxPicture pic = new IdxPicture(img, "0", "file", 0);
+		pic.subsample(1, 0.95, tanh);
+		if(pic.getData()[0] != tanh.compute(100.0))
+			System.out.println("Error in DVV-Test 01");
+		pic.subsample(1, 0.0, tanh);
+		if(pic.getData()[0] != tanh.compute(100.0))
+			System.out.println("Error in DVV-Test 01");
+		pic.subsample(0, 0.0, tanh);
+		pic.subsample(2, 0.0, tanh);
 	}
 
-	public static void test02() throws InvalidFileException {
+	public static void test02() {
+		byte[][] img = new byte[100][100];
+		for(int i=0; i<img.length; i++)
+			for(int j=0; j<img.length; j++)
+				img[i][j] = 50;
+		IdxPicture pic = new IdxPicture(img, "0", "file", 0);
+		for(int k=0; k<=95; k++) {
+			byte[][] data = pic.previewSubsampledData(23, k/100.0);
+			for(int i=0; i<data.length; i++)
+				for(int j=0; j<data.length; j++)
+					if(data[i][j] != 50)
+						System.out.println("Error in DVV-Test 02");
+		}
+	}
+
+	public static void test03() {
+		byte[][] img = new byte[10][10];
+		for(int i=0; i<img.length; i++)
+			for(int j=0; j<img.length; j++)
+				img[i][j] = 0;
+		for(int i=0; i<img.length; i++)
+			img[i][i] = 10;
+		IdxPicture pic = new IdxPicture(img, "0", "file", 0);
+		byte[][] data = pic.previewSubsampledData(5, 0.0);
+		for(int i=0; i<data.length; i++)
+			for(int j=0; j<data.length; j++) {
+				if(i==j && data[i][j] != 5)
+					System.out.println("Error in DVV-Test 03");
+				if(i!=j && data[i][j] != 0)
+					System.out.println("Error in DVV-Test 03");
+			}
+	}
+
+	public static void test() throws InvalidFileException {
 		DVV dvv = new DVV("images", "labels");
 		Collection<? extends Data> allData = dvv.getDataSet();
 		for(Data data : allData) {
-			byte[][] image = ((IdxPicture)data).previewRawData();
+			byte[][] image = ((IdxPicture)data).previewSubsampledData(10, 0);
 			System.out.println(data.getLabel());
 			for(int i=0; i<image.length; i++) {
 				for(int j=0; j<image[0].length; j++)
