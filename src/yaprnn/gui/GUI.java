9cd8a33ee4db0f04ea31e4211cecb0111f7d12e6
falkosch@gmx.de <falkosch@gmx.de@0986f430-494d-0410-a540-1573547e6c47>
2008-06-24 18:05:55 +0000

    GUI update

--- src/yaprnn/gui/GUI.java
+++ src/yaprnn/gui/GUI.java
@@ -2,96 +2,66 @@ package yaprnn.gui;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Vector;
-
 import yaprnn.Core;
+import yaprnn.GUIInterface;
 import yaprnn.dvv.Data;
 import yaprnn.gui.view.MainView;
 
 /**
- * GUI-Controller von yaprnn.
+ * GUI controller
  */
-public class GUI {
+public class GUI implements GUIInterface {
 
 	private Core core;
-	private MainView mainView = null;
-	private Collection<IGUIListener> listener = new Vector<IGUIListener>();
+	private MainView mainView;
 
 	/**
-	 * Konstruktor
+	 * Constructor
 	 * 
 	 * @param mv
-	 *            Die zu verwaltende MainView.
+	 *            the main view to be used
 	 */
 	public GUI(Core core, MainView mv) {
-		this.setView(mv);
-		mv.setVisible(true);
-	}
-
-	/**
-	 * Aktualisiert die Datenpunkte für den Graphen des Training-Fehlers.
-	 * 
-	 * @param errorData
-	 *            Die Training-Fehler-Datenpunkte.
-	 */
-	public void setTrainingError(List<Double> errorData) {
-		// TODO : GUI.setTrainingError
-	}
-
-	/**
-	 * Aktualisiert die Datenpunkte für den Graphen des Test-Fehlers.
-	 * 
-	 * @param errorData
-	 *            Die Test-Fehler-Datenpunkte.
-	 */
-	public void setTestError(List<Double> errorData) {
-		// TODO : GUI.setTestError
-	}
-
-	/**
-	 * Aktualisiert die Liste an verfügbaren DataSets in der MainView.
-	 * 
-	 * @param dataset
-	 *            Das dataset.
-	 */
-	public void setDataSet(Collection<Data> dataset) {
-		// TODO : GUI.setDataSet
-	}
-
-	/**
-	 * Setzt die zu verwaltene MainView.
-	 * 
-	 * @param mainView
-	 *            Die zu verwaltende MainView.
-	 */
-	public void setView(MainView mainView) {
-		if (this.mainView != null)
-			disconnectMainView();
-		this.mainView = mainView;
+		mainView = mv;
 		connectMainView();
+		mainView.setVisible(true);
+		core.setGUI(this);
 	}
 
 	/**
-	 * Liefert die aktuell verwaltete MainView.
+	 * Returns the MainView of the GUI.
+	 * 
+	 * @return the MainView
 	 */
-	public MainView getView() {
+	MainView getView() {
 		return this.mainView;
 	}
 
 	/**
-	 * Hängt die Eventhandler an eine MainView.
+	 * Connects the event handler to the main view.
 	 */
 	private void connectMainView() {
-		listener.add(new ToolImportActionListener(mainView));
+		// Add menu listeners
+		new MenuExitActionListener(mainView);
+
+		// Add button listeners
+		new ToolImportActionListener(mainView);
+
 	}
 
-	/**
-	 * Entfernt die Eventhandler von einer MainView.
-	 */
-	private void disconnectMainView() {
-		for (IGUIListener l : listener)
-			l.disconnect();
-		listener.clear();
+	@Override
+	public void setDataSet(Collection<Data> dataset) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void setTestError(List<Double> errorData) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void setTrainingError(List<Double> errorData) {
+		// TODO Auto-generated method stub
 	}
 
 }
