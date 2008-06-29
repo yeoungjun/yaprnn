957b845a6572253b5b2e6a6afe518c453e3467e3
falkosch@gmx.de <falkosch@gmx.de@0986f430-494d-0410-a540-1573547e6c47>
2008-06-29 22:53:41 +0000

    Einige neue ActionListener hinzugefügt.

---
+++ trunk/src/yaprnn/gui/NewMLPActionListener.java
@@ -0,0 +1,105 @@
+package yaprnn.gui;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import yaprnn.mlp.NeuralNetwork;
+
+class NewMLPActionListener implements ActionListener {
+
+	private GUI gui;
+
+	NewMLPActionListener(GUI gui) {
+		this.gui = gui;
+		gui.getView().getMenuNewMLP().addActionListener(this);
+		gui.getView().getToolNewMLP().addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int numLayers = 0;
+		int numNeurons = 0;
+		boolean autoEncoder = false;
+
+		// Für die Eingabe-Schleife.
+		boolean notSatisfied;
+
+		// Input Dialog vorbereiten.
+		JLabel messageText = new JLabel();
+		JTextField textField = new JTextField();
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (!Character.isDigit(e.getKeyChar()))
+					e.consume();
+			}
+		});
+		JPanel panel = new JPanel(new GridLayout(2, 1));
+		panel.add(messageText);
+		panel.add(textField);
+
+		// Anzahl der Schichten?
+		messageText
+				.setText("How many layers do you want?\n(please input a value greater then 0)");
+		notSatisfied = true;
+		while (notSatisfied) {
+			int ret = JOptionPane.showConfirmDialog(gui.getView(), panel,
+					"New MLP", JOptionPane.OK_CANCEL_OPTION);
+			if (ret == JOptionPane.CANCEL_OPTION)
+				return;
+			try {
+				numLayers = Integer.parseInt(textField.getText());
+				if (numLayers > 0)
+					notSatisfied = false;
+			} catch (Exception ex) {
+			}
+		}
+
+		// Anzahl der Neuronen pro Schicht?
+		messageText
+				.setText("How many neurons per layer do you want?\n(please input a value greater then 0)");
+		notSatisfied = true;
+		while (notSatisfied) {
+			int ret = JOptionPane.showConfirmDialog(gui.getView(), panel,
+					"New MLP", JOptionPane.OK_CANCEL_OPTION);
+			if (ret == JOptionPane.CANCEL_OPTION)
+				return;
+			try {
+				numNeurons = Integer.parseInt(textField.getText());
+				if (numNeurons > 0)
+					notSatisfied = false;
+			} catch (Exception ex) {
+			}
+		}
+
+		// AutoEncoder?
+		int ret = JOptionPane.showConfirmDialog(gui.getView(),
+				"Do you want to apply auto encoding initialization?",
+				"New MLP", JOptionPane.YES_NO_CANCEL_OPTION);
+		if (ret == JOptionPane.CANCEL_OPTION)
+			return;
+		autoEncoder = (ret == JOptionPane.YES_OPTION);
+
+		// Parameter ausfüllen
+		int[] layer = new int[numLayers];
+		int[] avf = new int[numLayers];
+		double[] bias = new double[numLayers];
+		for (int i = 0; i < numLayers; i++) {
+			// Annahme von Standardwerten
+			layer[i] = 2;
+			avf[i] = 0;
+			bias[i] = 0.2;
+		}
+
+		// TODO : MLP erstellen
+		// NeuralNetwork mlp = gui.getCore().newMLP(layer, avf, bias,
+		// autoEncoder);
+		// gui.getTreeModel().add(mlp);
+	}
+}
