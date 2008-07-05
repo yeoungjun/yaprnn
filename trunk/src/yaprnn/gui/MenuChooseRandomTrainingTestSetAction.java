a287948681b417bb69dc6409e4843848ece06940
falkosch@gmx.de <falkosch@gmx.de@0986f430-494d-0410-a540-1573547e6c47>
2008-07-05 00:16:39 +0000

    Training und Test sets können bestimmt werden

--- trunk/src/yaprnn/gui/MenuChooseRandomTrainingTestSetAction.java
+++ trunk/src/yaprnn/gui/MenuChooseRandomTrainingTestSetAction.java
@@ -1,7 +1,16 @@
 package yaprnn.gui;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import yaprnn.dvv.Data;
+import yaprnn.mlp.NeuralNetwork;
 
 class MenuChooseRandomTrainingTestSetAction implements ActionListener {
 
@@ -20,7 +29,61 @@ class MenuChooseRandomTrainingTestSetAction implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		NeuralNetwork n = gui.getSelectedNetwork();
+		if (n == null)
+			return;
+		int trainPerc = 75, testPerc = 20;
+
+		boolean notSatisfied;
+
+		// Input Dialog vorbereiten.
+		JPanel panel = new JPanel(new GridLayout(4, 1));
+		JSpinner optionTrainPerc = new JSpinner(new SpinnerNumberModel(70, 1,
+				99, 1));
+		JSpinner optionTestPerc = new JSpinner(new SpinnerNumberModel(20, 1,
+				99, 1));
+		panel.add(new JLabel("Trainingset size in percent"));
+		panel.add(optionTrainPerc);
+		panel.add(new JLabel("Testset size in percent"));
+		panel.add(optionTestPerc);
+
+		// Parameter anfragen
+		notSatisfied = true;
+		while (notSatisfied) {
+			int ret = JOptionPane.showConfirmDialog(gui.getView(), panel,
+					"Choosing random sets", JOptionPane.OK_CANCEL_OPTION);
+			if (ret == JOptionPane.CANCEL_OPTION)
+				return;
+			trainPerc = ((Integer) optionTrainPerc.getValue()).intValue();
+			testPerc = ((Integer) optionTestPerc.getValue()).intValue();
+			if (trainPerc + testPerc <= 100)
+				notSatisfied = false;
+			else
+				JOptionPane.showMessageDialog(gui.getView(),
+						"The sum of both values cannot exceed 100 percent.",
+						"Error", JOptionPane.ERROR_MESSAGE);
+		}
+
+		List<Data> dataList = gui.getTreeModel().getDatasets();
+
+		// Zuerst setzen wir alle wieder auf NotUsed
+		for (Data d : dataList) {
+			gui.getTreeModel().remove(d, n);
+			d.setNotUsed();
+		}
+
+		// Der Core wählt die Sets aus.
+		gui.getCore().chooseRandomTrainingData(trainPerc / 100d,
+				testPerc / 100d);
+
+		// Wir schauen nur noch nach welche wo gelandet sind.
+		for (Data d : gui.getTreeModel().getDatasets()) {
+			if (d.isTraining()) {
+				gui.getTreeModel().add(d, n, true);
+			} else if (d.isTest()) {
+				gui.getTreeModel().add(d, n, false);
+			}
+		}
 
 	}
 
