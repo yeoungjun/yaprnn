2eecb2f41567180913d54c1923ad16ea6dc54bbd
felix@frypage.de <felix@frypage.de@0986f430-494d-0410-a540-1573547e6c47>
2008-07-10 18:35:45 +0000

    JavaDOC updates

--- trunk/src/yaprnn/mlp/DynamicEtaAdjustment.java
+++ trunk/src/yaprnn/mlp/DynamicEtaAdjustment.java
@@ -1,15 +1,33 @@
 package yaprnn.mlp;
 
+/**
+ * This object adjusts the learning rate in against last computed error. 
+ */
+
 public class DynamicEtaAdjustment implements Eta {
 
 	double eta, lastError, signum, roh;
-	
+
+	/**
+	 * Constructor which sets the learning rate, signum and roh. Look at getEta for further information.
+	 * @param eta The learning rate.
+	 * @param signum Multiplier for a smaller learning rate.
+	 * @param roh Multiplier for a bigger learning rate.
+	 */
 	public DynamicEtaAdjustment(double eta, double signum, double roh) {
 		this.eta = eta;
 		this.signum = signum;
 		this.roh = roh;
 	}
 	
+	/**
+	 * Returns a adjusted learning rate.
+	 * If the delivered error is bigger than the saved one, the learning rate is adjusted with signum (mult.).
+	 * With a smaller error, than the saved one, the learning rate will be multiplied with roh.
+	 * Otherwise the learning rate won't be touched.  
+	 * @param lastError The last computed error.
+	 * @retun The adjusted learning rate.
+	 */
 	public double getEta(double lastError) {
 		
 		if(this.lastError > lastError)
@@ -20,7 +38,11 @@ public class DynamicEtaAdjustment implements Eta {
 		this.lastError = lastError;
 		return eta;
 	}
-
+	
+	/**
+	 * Returns a string-representation of the learning rate.
+	 * @return The learning rate as string.
+	 */
 	public String toString(){
 		return Double.toString(eta);
 	}
