4b3e2941f188375a5aa9dd51944a70d15728b4c0
felix@frypage.de <felix@frypage.de@0986f430-494d-0410-a540-1573547e6c47>
2008-07-11 09:32:49 +0000

    JavaDOC update; BatchSize eingeführt, fehlt noch in der TrainingView; Momentan auf 20 hardcoded (Update Schritt alle batchSize backpropagations). Batch-Modus nun nutzbar.

--- src/yaprnn/mlp/BadConfigException.java
+++ src/yaprnn/mlp/BadConfigException.java
@@ -4,27 +4,83 @@ public class BadConfigException extends Exception {
 	private static final long serialVersionUID = 1L;
 	private static int identifer = 0;
 	
+	/**
+	 * Identifer for a invalid neuron number
+	 */
 	public static int INVALID_NEURON_NUMBER = 1;
+	
+	/**
+	 * Identifer for a invalid activation function
+	 */
 	public static int INVALID_ACTIVATION_FUNCTION = 2;
+
+	/**
+	 * Identifer for a invalid input dimension
+	 */
 	public static int INVALID_INPUT_DIMENSION = 3;
+
+	/**
+	 * Identifer for a invalid input layer dimension
+	 */
 	public static int INVALID_INPUT_LAYER_DIMENSION = 4;
+
+	/**
+	 * Identifer for a invalid output layer dimension
+	 */
 	public static int INVALID_OUTPUT_LAYER_DIMENSION = 5;
+
+	/**
+	 * Identifer for a invalid eta
+	 */
 	public static int INVALID_ETA = 6;
+
+	/**
+	 * Identifer for a invalid number of values for the bias
+	 */
 	public static int INVALID_NUMBER_OF_BIAS = 7;
+	
+	/**
+	 * Identifer for a invalid number of activation functions
+	 */
 	public static int INVALID_NUMBER_OF_FUNCTIONS = 8;
+	
+	/**
+	 * Identifer for a invalid target vector
+	 */
 	public static int INVALID_TARGET_VECTOR = 9;
+	
+	/**
+	 * Identifer for a invalid error vector
+	 */
 	public static int INVALID_ERROR_VECTOR = 10;
+	
+	/**
+	 * Identifer for a problem caused by a missing dvv.
+	 */
 	public static int DVV_NOT_LOADED = 11;
 	
+	/**
+	 * Constructor which only sets the error message.
+	 * @param message The error message.
+	 */
 	public BadConfigException(String message) {
 		super(message);
 	}
 	
+	/**
+	 * This constructor sets the error message and a identifer describing the message.
+	 * @param message The error message.
+	 * @param id The corresponding error identifer.
+	 */
 	public BadConfigException(String message, int id) {
 		super(message);
 		identifer = id;
 	}
 	
+	/**
+	 * Returns the exceptions identifer.
+	 * @return The identifer corresponding to the error message.
+	 */
 	public int getId(){
 		return identifer;
 	}
