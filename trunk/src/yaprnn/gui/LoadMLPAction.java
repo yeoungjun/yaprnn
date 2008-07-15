d1dbe83f9219b3590aea6730afffe8f2e57eed70
falkosch@gmx.de <falkosch@gmx.de@0986f430-494d-0410-a540-1573547e6c47>
2008-07-15 17:20:19 +0000

    Reset Handler implementiert. Standardwerte beim erstellen eines MLPs verändert. LoadMLP und NewMLP löschen nun workaround mäßig das alte MLP.

--- trunk/src/yaprnn/gui/LoadMLPAction.java
+++ trunk/src/yaprnn/gui/LoadMLPAction.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
+import yaprnn.mlp.NeuralNetwork;
+
 class LoadMLPAction implements ActionListener {
 
 	private GUI gui;
@@ -17,15 +19,14 @@ class LoadMLPAction implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		NeuralNetwork n = null;
 		JFileChooser chooser = new JFileChooser();
 		chooser.setMultiSelectionEnabled(false);
 		chooser.setFileFilter(GUI.FILEFILTER_MLP);
 		if (chooser.showOpenDialog(gui.getView()) == JFileChooser.APPROVE_OPTION) {
 			try {
-				// MLP laden und zum TreeModel hinzufügen.
-				gui.getTreeModel().add(
-						gui.getCore().loadMLP(
-								chooser.getSelectedFile().getPath()));
+				// MLP laden
+				n = gui.getCore().loadMLP(chooser.getSelectedFile().getPath());
 			} catch (Exception ex) {
 				JOptionPane.showMessageDialog(gui.getView(),
 						"An error occured",
@@ -33,6 +34,16 @@ class LoadMLPAction implements ActionListener {
 								+ ex.getMessage(), JOptionPane.ERROR_MESSAGE);
 			}
 		}
+
+		if (n == null)
+			return;
+
+		// TODO : Da wir noch nicht mehrere MLPs unterstützen, löschen wir das
+		// gerade stehende MLP aus dem Baum!
+		if (gui.getTreeModel().getNetworks().size() > 0)
+			gui.getTreeModel().remove(gui.getTreeModel().getNetworks().get(0));
+
+		gui.getTreeModel().add(n);
 	}
 
 }
